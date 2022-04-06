f96445fca5ad156b85c6d90ec50cb47b25054714
johnnytrung567 <vantrung242628@gmail.com>
2022-04-06 18:00:16 +0700

    Fix server api for 2nd time

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:5000/api'
-        : 'https://quiet-river-55048.herokuapp.com'
+        : 'https://quiet-river-55048.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
